45c94384162afa78abc79d6f01fd018c42677b1c
srdokinch <srdokinch@gmail.com>
2025-12-17 23:56:10 +0900

    App.jsx編集、ブラウザで表示確認なぜかできず

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import CityInput from './components/CityInput'
 
 function App() {
   const [city, setCity] = useState('')
+
+  const handleSearch = (cityName) => {
+    console.log('検索：', cityName)  //動作確認用（後でAPI呼び出しに変更するよ）
+  }
   
   return (
     <div className="App">
@@ -10,7 +15,11 @@ function App() {
         <h1>天気予報アプリ</h1>
       </header>
       <main>
-        {/* ここにコンポーネントを追加していく */}
+        <CityInput
+          city={city}
+          setCity={setCity}
+          onSearch={handleSearch}
+        />
       </main>
     </div>
   )
--- src/components/CityInput.jsx
+++ src/components/CityInput.jsx
@@ -8,14 +8,14 @@ const CityInput = ({city, setCity, onSearch}) => {
 
   return (
     // フォームが送信された時にhandleSubmitが実行されブラウザが自動でイベント情報（e）を渡す
-    <form onSubmit={handleSubmit}>
+    <form className="city-input-form" onSubmit={handleSubmit}>
       <input 
         type="text"
         placeholder="都市名を入力してください（例：札幌, Sapporo）"
         value={city}
         onChange={(e) => setCity(e.target.value)}
       />
-      <button type="submit">検索</button>
+      <button className="search-button" type="submit">検索</button>
     </form>
   )
 }
