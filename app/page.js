bbbc187b72a6b7988b7b93dff4a29a4c8699a3ec
srdokinch <srdokinch@gmail.com>
2026-02-11 00:23:01 +0900

    チャプター3の手順書追加

--- app/page.js
+++ app/page.js
@@ -1,5 +1,8 @@
 "use client";
 
+import { useState } from "react";
+
+
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center gap-8 min-h-screen">
